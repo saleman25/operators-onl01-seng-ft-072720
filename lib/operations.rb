2ea6539ee9deebb00dcdf7a87e98381ced95754f
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-27 23:44:34 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,15 @@
 def unsafe?(speed)
-if speed < 40 || speed > 60
+if speed >= 60
   return true
-else return false
-  end
+else speed <= 40
+  return false
+end
 
 
 
 def not_safe?(speed)
-return (speed < 60 && speed > 40 ? true : false)
- # return (speed < 40 || speed > 60 ? true : false)
-#	speed < 40 || speed > 60 ? true : false
-# return speed > 60 || speed < 40 ? true : false
-  end
+	speed > 60 "safe" : "not safe"
+end
+	
+
+
